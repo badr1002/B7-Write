f329a5a8f44592a5e3fb657687309aaa59120de3
Badr Helal <64813913+badr1002@users.noreply.github.com>
2020-11-01 11:55:15 +0200

    Add files via upload

---
+++ precache-manifest.ae8dce98b57e431e8d533f21cc4940a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45410e9796b5f4fa9be4e06d875209e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29da177376df9d721d06",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "abb3eaae8d18b8280b74",
+    "url": "/static/css/main.4742c454.chunk.css"
+  },
+  {
+    "revision": "29da177376df9d721d06",
+    "url": "/static/js/2.896b72f1.chunk.js"
+  },
+  {
+    "revision": "455af8c4e7a7e2f83c05bc8d92949c7d",
+    "url": "/static/js/2.896b72f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb3eaae8d18b8280b74",
+    "url": "/static/js/main.3b5ece86.chunk.js"
+  },
+  {
+    "revision": "feccbdf4f566876d18ad",
+    "url": "/static/js/runtime-main.add56a83.js"
+  },
+  {
+    "revision": "1fc247b4f7bbca7ba6f0ef1d05b1a47b",
+    "url": "/static/media/avatar.1fc247b4.jpg"
+  },
+  {
+    "revision": "29029173b2152a6784826e93738b1a9f",
+    "url": "/static/media/code.29029173.jpg"
+  },
+  {
+    "revision": "05f591c9908f0d16f4d1c80eeeeecae8",
+    "url": "/static/media/hicit_logo1.05f591c9.png"
+  },
+  {
+    "revision": "f1896a2f4ce028f025cc3c487ee10712",
+    "url": "/static/media/hie.f1896a2f.png"
+  },
+  {
+    "revision": "3e840ba2b11a8610599af4cf2cd7030c",
+    "url": "/static/media/ihmi_logoweb.3e840ba2.png"
+  }
+]);
